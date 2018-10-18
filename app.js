2409d101d096d00043534ec262e316774be151f9
richcoleman <coleman.richardm@gmail.com>
2018-10-17 20:46:42 -0500

    adding changes to the app and test app

---
+++ app.js
@@ -0,0 +1,47 @@
+var countries = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.joinsherpa.com/v2/countries",
+    "method": "GET",
+    "headers": {
+        "Authorization": "Basic Zk5saHhmUTB1cm94YVhvQ1NuOUY2a0V3clFjQWN6OmtSaWFQTWhxTU5GbzdtMDVDbHY5S3IzVVBaNXNjWWg2TnJCaVZkTjQ="
+    }
+};
+
+
+$(".btn").click(function (e) {
+    e.preventDefault();
+    $.ajax(countries).done(function (response) {
+        console.log(response)
+        var destination = $("#destination-box").val().trim();
+        var citizenship = $("#citizenship-box").val().trim();
+        for (i = 0; i < response.length; i++) {
+            if (destination === response[i].country_name) {
+                var destinationID = response[i].country_ID;
+            }
+        }
+        for (i = 0; i < response.length; i++) {
+            if (citizenship === response[i].country_name) {
+                var citizenshipID = response[i].country_ID;
+            }
+        }
+        var visaRequirements = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://api.joinsherpa.com/v2/visa-requirements/" + citizenshipID + "-" + destinationID,
+            "method": "GET",
+            "headers": {
+               "Authorization": "Basic Zk5saHhmUTB1cm94YVhvQ1NuOUY2a0V3clFjQWN6OmtSaWFQTWhxTU5GbzdtMDVDbHY5S3IzVVBaNXNjWWg2TnJCaVZkTjQ="
+            }
+          };
+
+        $.ajax(visaRequirements).done(function (response) {
+            console.log(response);
+        })
+
+
+
+    })
+});
+
+
