293b8d6b2dc234065d852994ce0d8d0d412c3d71
richcoleman <coleman.richardm@gmail.com>
2018-10-20 14:03:44 -0500

    updated currency code to return whole number value)

--- Rich edits/testapp.js
+++ Rich edits/testapp.js
@@ -8,10 +8,16 @@ var countries = {
     }
 };
 
+var currencyCountryURL = "https://free.currencyconverterapi.com/api/v6/countries";
+
+
+
 
 $("#btn").click(function (e) {
+    $("#visa").empty();
     e.preventDefault();
     $.ajax(countries).done(function (response) {
+        console.log(response);
         var destination = $("#usercountry").val().trim();
         var citizenship = $("#usercitizenship").val().trim();
         for (i = 0; i < response.length; i++) {
@@ -24,27 +30,28 @@ $("#btn").click(function (e) {
                 var citizenshipID = response[i].country_ID;
             }
         }
-        var currencyCountryURL = "https://free.currencyconverterapi.com/api/v6/countries";
+
+
 
         $.ajax(currencyCountryURL).done(function (response) {
             var userCurrency = response.results[citizenshipID].currencyId;
             var destinationCurrency = response.results[destinationID].currencyId;
-            console.log("User Currency", userCurrency);
-            console.log("Destination Currency", destinationCurrency);
-
             var currencyDiv = $("#currency");
             currencyDiv.html("What's your budget? <br>  <input id='money' type='text'> <button class='button' id='moneybutton' data-value=''>Convert</button>");
             var moneyButton = $("#moneybutton");
-            $(moneyButton).click(function() {
+            $(moneyButton).click(function () {
                 var userBudget = $("#money").val();
-                console.log(userBudget);
-                var currencyConvertURL = "https://free.currencyconverterapi.com/api/v6/convert?q=" + userCurrency + "_" + destinationCurrency +","+ destinationCurrency + "_" + userCurrency + "&compact=ultra";
-                console.log(currencyConvertURL);
-                $.ajax(currencyConvertURL).done(function(currencyResponse){
-
+                var currencyConvertURL = "https://free.currencyconverterapi.com/api/v6/convert?q=" + userCurrency + "_" + destinationCurrency + "&compact=ultra";
+                $.ajax(currencyConvertURL).done(function (currencyResponse) {
                     console.log(currencyResponse);
-                })
-            });
+                    var newBudgetP = $("<p>");
+                    var conversion = Object.values(currencyResponse);
+                    var newBudget = Math.trunc(conversion * userBudget);
+                    newBudgetP.html("<br> Your travel budget will be around " + newBudget + " " + destinationCurrency)
+                    currencyDiv.append(newBudgetP);
+                });
+            })
+        });
 
         var visaRequirements = {
             "async": true,
@@ -56,26 +63,18 @@ $("#btn").click(function (e) {
             }
         };
 
+
         $.ajax(visaRequirements).done(function (response) {
-
-            //Console log to display paths
-            console.log(response.currency);
-            console.log(response.passport);
-            console.log(response.vaccination);
-            console.log(response.visa);
-            console.log(response.visa[0].allowed_stay);
-
-
-            console.log(response.visa[0].textual.text);
-            // console.log(response.visa[0].textual.text[1]);
-            // console.log(response.visa[0].textual.text[2]);
-
+            console.log(response);
+            var newP = $("<p>");
 
             //Write Visa info to visa div
             var visaDiv = $("#visa");
+            newP.html("-- " + response.visa[0].textual.text[0] + "<br> -- " + response.visa[0].textual.text[1] + "<br>-- " +
+                response.visa[0].textual.text[2]);
+            visaDiv.append(newP);
             // visaDiv.text(response.visa[0].type)
-            visaDiv.html("-- " + response.visa[0].textual.text[0] + "<br> -- " + response.visa[0].textual.text[1] + "<br>-- " +
-                response.visa[0].textual.text[2])
+
 
 
             //Write vaccination info to vax div
@@ -87,7 +86,7 @@ $("#btn").click(function (e) {
                 diseaseList.push(disease);
             }
 
-            if (diseaseList !== undefined) {
+            if (diseaseList !== undefined, null) {
                 vaxDiv.html("Before traveling, talk to your doctor about vaccinations against " + diseaseList + ".");
             } else if (!diseaseList) {
                 vaxDiv.text("No recommended vaccinations!");
@@ -98,30 +97,17 @@ $("#btn").click(function (e) {
 
             //Write length of stay info to length div
             var lengthDiv = $("#lengthofstay");
+            var currencyDiv = $("#currency");
+            var newCurrencyP = $("<p>");
             var allowedStay = response.visa[0].allowed_stay;
             var notesStay = response.visa[0].notes[0];
-            if (allowedStay === true) {
-                console.log("allowed stay is here")
-            } else if (notesStay === true) {
-                console.log("notes are here")
-            }
             lengthDiv.text(allowedStay, notesStay);
-
-
+            newCurrencyP.html("When you arrive and leave: " + response.currency.arrival)
+            currencyDiv.prepend(newCurrencyP);
         })
 
-
-
-
-
-
-
-        currencyDiv.html("When you arrive and leave: " + response.currency.arrival + "<br> What's your budget?")
     })
 
 
 
 })
-
-
-});
